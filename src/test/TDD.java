2814acb0a44193758ca9266dcd854d71bed94d5c
Anja Markovic <markovicanja98@gmail.com>
2022-11-21 15:13:59 +0100

    Added user data tests

--- src/test/TDD.java
+++ src/test/TDD.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import user.User;
 
@@ -17,9 +19,42 @@ class TDD {
 	}
 
 	@Test
-	void itShouldBeUniqueObject() {
+	void shouldBeUniqueObject() {
 		User user = User.getInstance();
+		
 		assertSame(testedUser, user);
+	}
+	
+	@Test
+	void setFirstNameTest() {
+		testedUser.setData("Anja", "Markovic", "anjamarkovic");
+		
+		assertEquals("Anja", testedUser.getFirstName());
+	}
+	
+	@Test
+	void setLastNameTest() {
+		testedUser.setData("Anja", "Markovic", "anjamarkovic");
+		
+		assertEquals("Markovic", testedUser.getLastName());
+	}
+	
+	@Test
+	void setUserNameTest() {
+		testedUser.setData("Anja", "Markovic", "anjamarkovic");
+		
+		assertEquals("anjamarkovic", testedUser.getUsername());
+	}
+	
+	@ParameterizedTest
+	@CsvSource({
+		"anjamarkovic, true",
+		"anja markovic, false"
+	})
+	void shoudValidateUsername(String username, boolean expected) {
+		boolean isValid = testedUser.setData("Anja", "Markovic", username);
+		
+		assertEquals(expected, isValid);
 	}
 
 }
