35a914647aa6e050dc7a630b78080f7514057ea9
Anja Markovic <markovicanja98@gmail.com>
2022-11-21 15:19:31 +0100

    Implemented user data methods

--- src/user/User.java
+++ src/user/User.java
@@ -4,8 +4,12 @@ public class User {
 	
 	private static User uniqueInstance = null;
 	
+	private String firstName, lastName, username;
+	
 	protected User() {
-		
+		firstName = "";
+		lastName = "";
+		username = "";
 	}
 	
 	public static User getInstance() {
@@ -13,6 +17,26 @@ public class User {
 			uniqueInstance = new User();
 		}
 		return uniqueInstance;
+	}
+
+	public boolean setData(String firstName, String lastName, String username) {
+		if (username.contains(" ")) return false;
+		uniqueInstance.firstName = firstName;
+		uniqueInstance.lastName = lastName;
+		uniqueInstance.username = username;
+		return true;
+	}
+
+	public String getFirstName() {
+		return uniqueInstance.firstName;
+	}
+
+	public String getLastName() {
+		return uniqueInstance.lastName;
+	}
+
+	public String getUsername() {
+		return uniqueInstance.username;
 	}
 
 }
